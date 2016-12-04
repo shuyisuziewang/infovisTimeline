99f78e9a85e274249e73028f50e37e185e9d4798
Suzie Wang <suziewang@gatech.edu>
2016-12-04 00:27:18 -0500

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -4,78 +4,80 @@ var config = {
     databaseURL: "https://infovis-f356e.firebaseio.com",
     storageBucket: "infovis-f356e.appspot.com",
     messagingSenderId: "338990255095"
-  };
-  firebase.initializeApp(config);
+};
+firebase.initializeApp(config);
 
-  var ref = firebase.database().ref("cases");
+var ref = firebase.database().ref("cases");
 
-var margin = {top: 20, right: 20, bottom: 30, left: 20},
-      width = 1024 - margin.left - margin.right,
-      height = 300 - margin.top - margin.bottom;
-
-
-  var x = d3.scaleLinear()
-      .range([0, width]);
-
-  var y = d3.scaleLinear()
-      .range([height, 0]);
-
-  var svg = d3.select("body").append("svg")
-      .attr("width", width)
-      .attr("height", height)
-      .append("g")
-      .attr("transform", "translate("+margin.left+","+margin.top+")")
-
-  var line = d3.line()
-      .curve(d3.curveBasis)
-      .x(function(d) { return x(d.key); })
-      .y(function(d) { return y(d.value); })
+var margin = { top: 20, right: 20, bottom: 50, left: 20 },
+    width = 1024 - margin.left - margin.right,
+    height = 500 - margin.top - margin.bottom;
 
 
-  ref.orderByKey().startAt("201102230007").on("value", function(error, csvdata){
-      
-      var casesSum = d3.nest()
-      .key(function(d) { 
+var x = d3.scaleLinear()
+    .range([0, width]);
 
-        console.log (d.iyear);
-        return d.year; 
-      })
-      .rollup(function(t) { return t.length; })
-      .entries(csvdata);
+var y = d3.scaleLinear()
+    .range([height, 0]);
 
-      console.log(casesSum)
+var svg = d3.select("body").append("svg")
+    .attr("width", width)
+    .attr("height", height)
+    .append("g")
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")")
 
-      casesSum.forEach(function(d){
+var line = d3.line()
+    .curve(d3.curveBasis)
+    .x(function(d) {
+        return x(d.key); })
+    .y(function(d) {
+        return y(d.value); })
+
+
+ref.orderByKey().startAt("199001000001").on("value", function(snapshot) {
+
+    var cache = Object.values(snapshot.val());
+
+    var casesSum = d3.nest()
+        .key(function(d) { return d.iyear;})
+        .rollup(function(t) { return t.length; })
+        .entries(cache);
+
+    console.log(casesSum);
+
+    casesSum.forEach(function(d) {
         d.key = +d.key;
         d.value = +d.value;
         console.log(d);
         return d;
-        })
+    })
 
-      x.domain(d3.extent(casesSum, function(d) { console.log(d.key); return d.key; }));
-      y.domain(d3.extent(casesSum, function(d) { return d.value; }));
+    x.domain(d3.extent(casesSum, function(d) { console.log(d.key);
+        return d.key; }));
+    y.domain(d3.extent(casesSum, function(d) {
+        return d.value; }));
 
-      // add line path
-      svg.append("path")
-         .datum(casesSum)
-         .attr("class", "line")
-         .attr("d", line);
+    // add line path
+    svg.append("path")
+        .datum(casesSum)
+        .attr("class", "line")
+        .attr("d", line);
 
-      //add X axis
-      svg.append("g")
-         .attr("class", "axis axis--x")
-         .attr("transform", "translate(0," + height + ")")
-         .call(d3.axisBottom(x));
+    //add X axis
+    svg.append("g")
+        .attr("class", "axis axis--x")
+        .attr("transform", "translate(0," + height + ")")
+        .call(d3.axisBottom(x));
 
-      //add Y axis
-      svg.append("g")
-         .attr("class", "axis axis--y")
-         .call(d3.axisLeft(y))
+    //add Y axis
+    svg.append("g")
+        .attr("class", "axis axis--y")
+        .call(d3.axisLeft(y))
         .append("text")
-         .attr("fill", "#000")
-         .attr("transform", "rotate(-90)")
-         .attr("y", 6)
-         .attr("dy", "0.71em")
-         .style("text-anchor", "end")
-         .text("Cases");
-    });
+        .attr("fill", "#000")
+        .attr("transform", "rotate(-90)")
+        .attr("y", 6)
+        .attr("dy", "0.71em")
+        .style("text-anchor", "end")
+        .text("Cases");
+});
